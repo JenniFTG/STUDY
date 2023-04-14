0061c9dc2ca95d4550716da8ea31cc683538d868
JenniFTG <jennilee.ftg@gmail.com>
2023-04-14 15:43:45 +0900

    SECTION8 MONGODB Limiting Fields & Pagination ~Lecture 99

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -31,6 +31,17 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v'); //excluding only v
     }
 
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    // page=2&limit=2, 1-10 page1, 11-20 page2 21-30 page3
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist!!');
+    }
     // EXECUTE QUERY
     const tours = await query;
 
