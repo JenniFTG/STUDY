3f48f72ccf7ca6cbdee0b4548e28152fc46f5ac3
JenniFTG <jennilee.ftg@gmail.com>
2023-04-14 11:50:40 +0900

    Section8 field limiting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -17,11 +17,20 @@ exports.getAllTours = async (req, res) => {
 
     // 2) Sorting
     if (req.query.sort) {
-      const sortBy = req.query.sort.split('.').join(' ');
+      const sortBy = req.query.sort.split(',').join(' ');
       query = query.sort(sortBy);
     } else {
       query = query.sort('-createdAt');
     }
+
+    // 3) Field limiting
+    if (req.query.fields) {
+      const fileds = req.query.fields.split(',').join(' ');
+      query = query.select(fileds);
+    } else {
+      query = query.select('-__v'); //excluding only v
+    }
+
     // EXECUTE QUERY
     const tours = await query;
 
